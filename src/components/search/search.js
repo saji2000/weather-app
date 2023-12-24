28b6787ded65eb5220e4e2f43f2ce86bcf737414
saji2000 <sajaddaneshmand79@gmail.com>
2023-12-23 23:31:24 -0700

    there is an issue with the API key

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,8 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
+import { GEO_API_URL, geoApiOptions } from "../../api";
 
 const Search = ({ onSearchChange }) => {
   const [search, setSearch] = useState(null);
+
+  useEffect(() => {
+    console.log(GEO_API_URL, geoApiOptions);
+  });
+
+  const loadOptions = (inputValue) => {
+    return fetch(
+      `${GEO_API_URL}/cities?minPopulation=100000&namePrefix=${inputValue}`,
+      geoApiOptions
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((json) => {
+        if (!json.data) {
+          throw new Error("Response JSON did not contain data property");
+        }
+        return {
+          options: json.data.map((city) => ({
+            value: `${city.latitude} ${city.longitude}`,
+            label: `${city.name}, ${city.countryCode}`,
+          })),
+        };
+      })
+      .catch((err) => {
+        console.error(err);
+        return { options: [] };
+      });
+  };
 
   const handleOnChange = (searchData) => {
     setSearch(searchData);
@@ -15,6 +48,7 @@ const Search = ({ onSearchChange }) => {
       debounceTimeout={600}
       value={search}
       onChange={handleOnChange}
+      loadOptions={loadOptions}
     />
   );
 };
