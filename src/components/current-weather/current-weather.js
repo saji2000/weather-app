dda27b8837a7b681f839b8c6c00a280273e094de
saji2000 <sajaddaneshmand79@gmail.com>
2023-12-25 15:51:18 -0700

    weather card sample, thank God

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -4,10 +4,36 @@ const CurrentWeather = () => {
   return (
     <div className="weather">
       <div className="top">
-        <p className="city"> London</p>
-        <p className="weather-description"> Sunny</p>
+        <div>
+          <p className="city"> London</p>
+          <p className="weather-description"> Sunny</p>
+        </div>
+        <img alt="weather" className="weather-icon" src="icons/01d.png" />
       </div>
-      <img alt="weather" className="weather-icon" src="icons/01d.png" />
+      <div className="bottom">
+        <p className="temperature">18 °C</p>
+        <div className="details">
+          <div className="paramater-row">
+            <span className="paramater-label">Details</span>
+          </div>
+          <div className="paramater-row">
+            <span className="paramater-label">Feels like</span>
+            <span className="paramater-value">22 °C</span>
+          </div>
+          <div className="paramater-row">
+            <span className="paramater-label">Wind</span>
+            <span className="paramater-value">4 km/h</span>
+          </div>
+          <div className="paramater-row">
+            <span className="paramater-label">Humidity</span>
+            <span className="paramater-value">15%</span>
+          </div>
+          <div className="paramater-row">
+            <span className="paramater-label">Pressure</span>
+            <span className="paramater-value">12 hPa</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
