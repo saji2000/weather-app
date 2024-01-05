172e761767247619afdd2852c94a96365755c5dd
saji2000 <sajaddaneshmand79@gmail.com>
2024-01-04 18:52:56 -0700

    Weather project finished, thank God

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -42,17 +42,42 @@ const Forecast = ({ data }) => {
                   />
                   <label className="day">{forecastDays[idx]}</label>
                   <label className="description">
-                    {/* <CurrentWeather data={data} /> */}
                     {item.weather[0].description}
                   </label>
-                  <label className="min-max">
-                    {Math.round(item.main.temp_min)}°C /
-                    {Math.round(item.main.temp_max)}°C
+                  <label className="forecast-temp">
+                    {Math.round(item.main.temp_min)}°C
                   </label>
                 </div>
               </AccordionItemButton>
             </AccordionItemHeading>
-            <AccordionItemPanel></AccordionItemPanel>
+            <AccordionItemPanel>
+              <div className="daily-details-grid">
+                <div className="daily-details-grid-item">
+                  <label>Pressure </label>
+                  <label>{item.main.pressure} hPa</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Humidity </label>
+                  <label>{item.main.humidity}</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Clouds </label>
+                  <label>{item.clouds.all}%</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Wind Speed </label>
+                  <label>{item.wind.speed}</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Sea Level </label>
+                  <label>{item.main.sea_level}m</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Feels Like </label>
+                  <label>{Math.round(item.main.feels_like)}°C</label>
+                </div>
+              </div>
+            </AccordionItemPanel>
           </AccordionItem>
         ))}
       </Accordion>
